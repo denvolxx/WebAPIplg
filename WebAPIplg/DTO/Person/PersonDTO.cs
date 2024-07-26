f7f8bf41b0ff72805e798781c56e8220f945f42c
Denys Fostytskyi <denys.fostytskyi@outlook.com>
2024-07-26 17:56:12 +0300

    DTO update

--- WebAPIplg/DTO/Person/PersonDTO.cs
+++ WebAPIplg/DTO/Person/PersonDTO.cs
@@ -1,10 +1,13 @@
-﻿using WebAPIplg.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using WebAPIplg.Models;
 
 namespace WebAPIplg.DTO.Person
 {
     public class PersonDTO
     {
+        [Required]
         public int Id { get; set; }
+
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public Gender? Gender { get; set; }
